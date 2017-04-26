57f0f1abbd37c251462e3c4c74dc8d535992d34d
Arseny Zorin <zorinarseny@yandex.ru>
2017-04-26 21:22:43 +0300

    DBus calls (Select modem, enable modem, dial number). Read the comment at the top of mainwindow.cpp

--- src/phone/mainwindow.cpp
+++ src/phone/mainwindow.cpp
@@ -1,19 +1,21 @@
+/*
+    КОСТЫЛИ. КОСТЫЛИ ВЕЗДЕ
+
+    Реализованы следующие методы:
+    Получение модема (Костыли. Нужна правильная обработка ответа)
+    Подключение модема (дождаться ответа. Не открывать GUI без ответа)
+    Звонок (После нажатия на кнопку перерисовать на совершение звонка. Дождаться ответа. После ответа добавление таймера вызова)
+*/
 
 #include "mainwindow.h"
-#include <QString>
-#include <QtDBus/QtDBus>
-#include <QDBusConnection>
-#include <QDBusMessage>
 
 MainWindow::MainWindow(QObject *parent)
     : QQmlApplicationEngine(parent)
 {
-	//setFocusPolicy(Qt::StrongFocus);
-	//setResizeMode(QDeclarativeView::SizeRootObjectToView);
-	//setSource(QUrl("qrc:///main.qml"));
-	//rootContext()->setContextProperty("window", this);
 	load(QUrl("qrc:///main.qml"));
 	rootContext()->setContextProperty("window", this);
+    GetModem();
+    EnableModem();
 }
 
 MainWindow::~MainWindow()
@@ -21,9 +23,71 @@ MainWindow::~MainWindow()
 
 }
 
-void MainWindow::EmptyFunc()
+void MainWindow::AnswerValidation(QDBusMessage msg)
 {
-	QApplication::quit();
+    if(QDBusMessage::ErrorMessage == msg.type()){
+        qDebug() << msg.errorMessage();
+        exit(1);
+    }
+}
+
+void MainWindow::GetModem()
+{
+    if(!bus.isConnected())
+        exit(1);
+
+    QDBusInterface dbus_iface("org.ofono", "/", "org.ofono.Manager", bus);
+
+    QDBusMessage modem = dbus_iface.call("GetModems");
+
+    AnswerValidation(modem);
+
+    //qDebug() << modem;
+
+    const QDBusArgument &dbusArgs = modem.arguments().first().value<QDBusArgument>();
+
+    if (dbusArgs.currentType() != QDBusArgument::ArrayType)
+    {
+        qDebug() << "Not array";
+        exit(1);
+    }
+
+    dbusArgs.beginArray();
+    while (!dbusArgs.atEnd())
+    {
+        dbusArgs.beginStructure();
+        dbusArgs >> selected_modem;
+        dbusArgs.endStructure();
+
+        if(selected_modem.contains("sim900"))
+            break;
+    }
+    dbusArgs.endArray();
+
+    qDebug() << "Selected modem: " << selected_modem;
+
+}
+
+
+void MainWindow::EnableModem()
+{
+    if(!bus.isConnected())
+        exit(1);
+
+    if(selected_modem.isNull() || selected_modem.isEmpty())
+    {
+        qDebug() << "No modem to enable";
+        exit(1);
+    }
+
+    QDBusInterface dbus_iface("org.ofono", selected_modem, "org.ofono.Modem", bus);
+
+    QDBusMessage modem_status = dbus_iface.call("SetProperty", QString("Powered"), QVariant::fromValue(QDBusVariant(true)));
+
+    AnswerValidation(modem_status);
+
+    qDebug() << modem_status;
+
 }
 
 void MainWindow::DialNumber(QString number)
@@ -31,35 +95,15 @@ void MainWindow::DialNumber(QString number)
 	if(number.isEmpty() || number.isNull())
 		return;
 
-	QDBusConnection bus = QDBusConnection::systemBus();
-	
-	if(!bus.isConnected())
-		fprintf(stderr, "Couldn't connect");
+    if(!bus.isConnected())
+        exit(1);
 
-	QDBusInterface dbus_iface("org.ofono", "/", "org.ofono.Manager", bus);
+    QDBusInterface dbus_iface("org.ofono", selected_modem, "org.ofono.VoiceCallManager", bus);
 
-	QDBusMessage modem = dbus_iface.call("GetModems");
+    QDBusMessage call_status = dbus_iface.call("Dial", number, QString("default"));
 
-	if(QDBusMessage::ErrorMessage == modem.type()){
-		qDebug() << modem.errorMessage();
-		return;
-	}
-	
-	qDebug() << modem;
-	qDebug() << modem.path();
+    AnswerValidation(call_status);
 
-	/*const QDBusArgument &dbusArgs = */qDebug() << modem.arguments().first();//.value<QDBusArgument>();
+    qDebug() << call_status;
 
-	//qDebug() << dbusArgs;
-
-	//QDBusObjectPath path;
-	//qDebug() << dbusArgs.currentType();
-    /*dbusArgs.beginArray();
-    while (!dbusArgs.atEnd())
-    {
-        dbusArgs >> path;
-    }
-    dbusArgs.endArray();*/
-    //dbusArgs >> path;
-    //qDebug() << path.path();
 }
--- src/phone/mainwindow.h
+++ src/phone/mainwindow.h
@@ -1,20 +1,27 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QtWidgets/QMainWindow>
 #include <QtCore/QUrl>
 #include <QtQml/QQmlApplicationEngine>
-#include <QtQuick/QQuickView>
 #include <QQmlContext>
 #include <QApplication>
-#include <QWidget>
+#include <QString>
+#include <QtDBus>
+
 class MainWindow : public QQmlApplicationEngine{
     Q_OBJECT
     
 public:
     MainWindow(QObject *parent = 0);
     ~MainWindow();
-    Q_INVOKABLE void EmptyFunc();
+    void GetModem();
+    void EnableModem();
+    //void TestConnection();
+    void AnswerValidation(QDBusMessage msg);
     Q_INVOKABLE void DialNumber(QString number);
+
+private:
+    QDBusConnection bus = QDBusConnection::systemBus();
+    QString selected_modem;
 };
 #endif // MAINWINDOW_H
