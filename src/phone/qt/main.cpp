2c1121608efe5a17ac07be3a04af373524fcdeb9
Arseny Zorin <zorinarseny@yandex.ru>
2017-07-05 14:44:27 +0300

    Parsing required argument of sim-module path

--- src/phone/qt/main.cpp
+++ src/phone/qt/main.cpp
@@ -1,9 +1,18 @@
+#include <iostream>
+#include <QDebug>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    if(argc < 2) {
+        qDebug() << "To start dialerApp you need to specify sim-module for call making\nFormat: \\sim900_0";
+        exit(1);
+    }
     QApplication a(argc, argv);
-    MainWindow w;
-    w.showDialer();
+    if(argc == 2) {
+        MainWindow w;
+        w.setSim(argv[1]);
+        w.showDialer();
+    }
     return a.exec();
 }
