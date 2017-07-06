c92e828db68bbad913743c11dc892ce68507464b
Arseny Zorin <zorinarseny@yandex.ru>
2017-07-07 00:05:31 +0300

    getSimPath function and DBus methods calls

--- src/phone/qt/Additional.h
+++ src/phone/qt/Additional.h
@@ -11,9 +11,42 @@
 #include <QScreen>
 #include <QPalette>
 #include <QWidget>
+#include <QtDBus/QDBusConnection>
+#include <QtDBus/QDBusInterface>
+#include <sys/stat.h>
 
 
 static std::string workingDir = "~//Repos//KSPT//Phone//src//phone//qt//";
+static QDBusConnection bus = QDBusConnection::systemBus();
+
+static QString getSimPath(){
+    struct stat buf;
+
+    if(stat(std::string("/usr/share/phonepi/info").c_str(), &buf) != 0){
+        qDebug() << "Cannot find file /usr/share/pnonepi/info";
+        qDebug() << "For correct work of dialer app sim-module is required";
+        exit(1);
+    }
+
+    char buffer[20];
+    std::string result = "";
+    FILE* pipe = popen(
+            "cat /usr/share/phonepi/info | grep Modem | awk '{ print $2 }'", "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    QString selected_modem = result.c_str();
+    return selected_modem = selected_modem.toStdString()
+            .substr(0, selected_modem.toStdString().size() -1).c_str();
+}
 
 static std::string get_fullpath(std::string path) {
     if (path[0] == '~') {
@@ -43,6 +76,29 @@ static QPalette getCommonPalette(QWidget* widget){
     pal.setColor(QPalette::Background, "#fbf1c7");
     return pal;
 }
+
+static void dialNumber(QString number){
+    if(!bus.isConnected()){
+        qDebug() << "Connection is not established";
+        exit(1);
+    }
+
+    QDBusInterface dbus_iface("org.ofono", getSimPath(), "org.ofono.VoiceCallManager", bus);
+    dbus_iface.call(QDBus::Block, "Dial", QVariant::fromValue(QString(number)),
+                    QVariant::fromValue(QString("")));
+}
+
+static void hangUp(){
+    if(!bus.isConnected()){
+        qDebug() << "Connection is not established";
+        exit(1);
+    }
+
+    QDBusInterface dbus_iface("org.ofono", getSimPath(), "org.ofono.VoiceCallManager", bus);
+    dbus_iface.call(QDBus::Block, "HangupAll");
+}
+
+
 
 
 
