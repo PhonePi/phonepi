609d8f7824b26e060dfb0026fc9ca5843dd2a520
Arseny Zorin <zorinarseny@yandex.ru>
2017-07-07 15:15:02 +0300

    Parsing of voice call path for answering on it

--- src/phone/qt/main.cpp
+++ src/phone/qt/main.cpp
@@ -5,13 +5,20 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    if(argc < 2) {
+    if(argc == 1) {
         DialerWindow *w = new DialerWindow();
         w->showDialer();
-    }else{
-        IncomingCall *incoming = new IncomingCall(argv[1]);
+    }else if(argc == 3){
+        IncomingCall *incoming = new IncomingCall(argv[1], argv[2]);
         incoming->showIncoming();
+    }else{
+        qDebug() << "ERROR: Incorrect use of dialer-pi.";
+        qDebug() << "For dial number no arguments requred";
+        qDebug() << "For answer call required: incoming call path and incoming number";
+        qDebug() << "Format: ./dialer-pi <path> <number>";
+        exit(1);
     }
+
 
     return a.exec();
 }
