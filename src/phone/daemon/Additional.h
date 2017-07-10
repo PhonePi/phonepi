155cf5d642d09cedb2d08ddaa37715044d3dd976
Arseny Zorin <zorinarseny@yandex.ru>
2017-07-10 15:12:51 +0300

    exec method for execution of external call

--- src/phone/daemon/Additional.h
+++ src/phone/daemon/Additional.h
@@ -40,5 +40,23 @@ static void writeLog(const char* message, int status) {
     closelog();
 }
 
+static std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
+}
+
 #endif
 //DAEMON_STRUCT_H
